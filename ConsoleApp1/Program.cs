c24ffd9a5a619763708f07ae7a1628ffbfda612a
JollyFoxy <pavel.mizirev@gmail.com>
2022-01-24 09:54:22 +0300

    LR

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ducks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck duck1 = new RedHeadDuck();
+            Duck duck2 = new MallardDuck();
+
+            Duck[] mas = new Duck[] { duck1, duck2 };
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i].Swim());
+                Console.WriteLine(mas[i].Display());
+                Console.WriteLine(mas[i].Quack());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
